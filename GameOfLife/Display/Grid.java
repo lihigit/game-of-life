dbb7f74af334f76246b9652081cb5728eba6b8f7
Lihi V <72017906+lihigit@users.noreply.github.com>
2021-04-11 16:02:59 +0300

    Update Grid.java

--- GameOfLife/Display/Grid.java
+++ GameOfLife/Display/Grid.java
@@ -28,13 +28,11 @@ class Grid extends JPanel {
         setLayout(layout);
         setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, this.GAP));
         setBackground(Color.DARK_GRAY);
-        int cellSize = (int) size / dimensions - 2 * size;
 
         // Initializes the cells:
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
                 this.cells[i][j] = new JPanel();
-                this.cells[i][j].setSize(cellSize, cellSize);
                 this.cells[i][j].setBorder(BorderFactory.createLineBorder(Color.WHITE, this.GAP/2));  // The GAP is divided by 2 since the gap between cells is 2 * cell[i][j].thickness.
                 add(this.cells[i][j]);
             }
